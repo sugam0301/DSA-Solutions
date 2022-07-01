2dba970907379e849683a6edd648cd2b414c7338
Sugam Mehta <66478859+sugam0301@users.noreply.github.com>
2022-07-02 01:11:53 +0530

    Time: 19 ms (89.98%), Space: 24.5 MB (87.38%) - LeetHub

---
+++ 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxi=0;
+    int  solve(TreeNode *root, unordered_map<int,int>&mp)
+    {
+        if(root==NULL) return 0;
+        int sum = root->val + solve(root->left,mp) + solve(root->right,mp);
+        mp[sum]++;
+        maxi=max(maxi,mp[sum]);
+        return sum;
+    }
+    
+    
+    vector<int> findFrequentTreeSum(TreeNode* root) 
+    {
+        vector<int>v;
+        unordered_map<int,int>mp;
+        solve(root,mp);
+        for(auto x:mp)
+            if(x.second==maxi)
+                v.push_back(x.first);
+        return v;
+        
+    }
+};
