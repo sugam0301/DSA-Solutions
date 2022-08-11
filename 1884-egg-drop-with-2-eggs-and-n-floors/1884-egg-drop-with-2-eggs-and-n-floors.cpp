d93f444081ce9b4e42cc2acdcfea7e641141d03e
Sugam Mehta <66478859+sugam0301@users.noreply.github.com>
2022-08-11 18:53:06 +0530

    Time: 8 ms (65.02%), Space: 5.9 MB (90.31%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int dp[1001][3];
+    int f(int k, int n)
+    {
+        if(n==1)    return k;
+        if(k==0 or k==1)    return k;
+        
+        if(dp[k][n]!=-1)    return dp[k][n];
+        
+        int ans = INT_MAX;
+        int low = 0, high = k;
+        
+        while(low<=high)
+        {            
+            int mid = (low + high)/2;
+            
+            int down = f(mid-1, n-1);
+            int up = f(k-mid, n);
+            
+            int attempt = 1 + max(down, up);
+            
+            if(down<up)
+                low = mid+1;
+            else
+                high = mid-1;
+            
+            ans = min(ans, attempt);
+        }        
+        return dp[k][n] =  ans;
+    }
+ 
+public:
+    int twoEggDrop(int n) {
+        memset(dp, -1, sizeof(dp));
+        return f(n,2);
+    }
+};
