11a5abe7e07b45d45994346c01c14b4d37c08caa
Sugam Mehta <66478859+sugam0301@users.noreply.github.com>
2023-09-04 20:37:08 +0530

    Added solution - LeetHub

---
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+private:
+    void dfs1(vector<vector<char>>&mat, int n, int m,int x,int y)
+    {
+        if(x<0 || y<0 || x>=n || y>=m)
+            return ;
+            
+        if(mat[x][y]!='O')
+            return;
+        if(mat[x][y]=='O')
+            mat[x][y]='-';
+        
+        
+        dfs1(mat,n,m,x+1,y);
+        dfs1(mat,n,m,x-1,y);
+        dfs1(mat,n,m,x,y-1);
+        dfs1(mat,n,m,x,y+1);
+    }
+    
+    
+public:
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+    {
+        // vector<vector<char>>res;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if((i*j)==0 || i==n-1 || j==m-1)
+                    dfs1(mat,n,m,i,j);
+            }
+        }
+        
+        for(int i =0; i < n; i++)
+        {
+            for(int j =0; j < m; j++)
+            {
+                if(mat[i][j]=='-')
+                    mat[i][j]='O';
+                else if(mat[i][j]=='O')
+                    mat[i][j]='X';
+                
+            }
+        }
+        return mat;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<char>> mat(n, vector<char>(m, '.'));
+        for(int i = 0;i < n;i++)
+            for(int j=0; j<m; j++)
+                cin>>mat[i][j];
+        
+        Solution ob;
+        vector<vector<char>> ans = ob.fill(n, m, mat);
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < m;j++) {
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
